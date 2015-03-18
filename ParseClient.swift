c7ae8f5fe0cbd95a29777615ee699df4a5eb588b
Ryan Newton <rnewton@hearsaycorp.com>
2015-03-18 13:28:54 -0700

    Re-add missing stuff

--- ParseClient.swift
+++ ParseClient.swift
@@ -39,6 +39,54 @@ class ParseClient {
         }
         return myEvents
     }
+    
+    class func setupParse() {
+        //        Parse.enableLocalDatastore()
+        Parse.setApplicationId("Oz5dstQ42Z3UQoau7JdbIZaS1PJLo3JyDaOU8cMd",
+            clientKey: "VZpD5J8u6azzxkvHTGbhNe2uJpusto5aHzPobNiF")
+        PFUser.enableAutomaticUser()
+        // If you would like all objects to be private by default, remove this line.
+        var defaultACL = PFACL()
+        defaultACL.setPublicReadAccess(true)
+        PFACL.setDefaultACL(defaultACL, withAccessForCurrentUser: true)
+        
+    }
+    
+    class func loadEventWithCallback(callback: (event: Event?) -> Void) {
+        var test = PFQuery(className: "Event")
+        var objs = test.findObjects()
+        var query = Event.query()
+        
+        query.getFirstObjectInBackgroundWithBlock {
+            (object: PFObject!, error: NSError!) -> Void in
+            if error == nil {
+                // The find succeeded.
+                println("Successfully retrieved an event.")
+                if let event = object as? Event {
+                    var projectPointers = event["projects"] as [PFObject]
+                    event.projects = [Project]()
+                    for projectPointer in projectPointers {
+                        var projectQuery = Project.query()
+                        var projectResponse = projectQuery.getObjectWithId(projectPointer.objectId)
+                        
+                        if let project = projectResponse as? Project {
+                            event.projects.append(project)
+                        } else {
+                            println("That was not a project :(!")
+                        }
+                    }
+                    callback(event: event)
+                }
+                
+                
+            } else {
+                // Log details of the failure
+                println("Error: \(error) \(error.userInfo!)")
+            }
+            
+        }
+    }
+    
     class func loadEvents() -> [Event] {
         var events = [Event]()
         var query = Event.query()
--- samahope/AppDelegate.swift
+++ samahope/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
         ParseClient.setupParse()
 
         let storyboard = UIStoryboard(name: "ProgramStoryboard", bundle: nil)
