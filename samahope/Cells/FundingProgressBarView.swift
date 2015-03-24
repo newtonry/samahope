878ff9464aa266f1772960f5f958aae057279efc
Ryan Newton <rnewton@hearsaycorp.com>
2015-03-23 18:28:55 -0700

    Clear subviews from progress bar on reuse

--- samahope/Cells/FundingProgressBarView.swift
+++ samahope/Cells/FundingProgressBarView.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class FundingProgressBarView: UIView {
     func fillWithPercent(fillPerc: CGFloat) {
+        for subview in self.subviews {
+            subview.removeFromSuperview()
+        }
+        
         self.backgroundColor = UIColor(red: 0.949, green: 0.941, blue: 0.929, alpha:1.0)
         let progressFillWidth = self.frame.width * fillPerc
         var progressFillFrame = CGRect(x: 0, y: 0, width: progressFillWidth, height: self.frame.height)
